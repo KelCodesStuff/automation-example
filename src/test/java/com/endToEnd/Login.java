878be8824cc76494c2664783fbaf14441c7c951e
Kelvin Reid <codesmithk@gmail.com>
2020-10-05 11:07:29 -0400

    Update Login.java

--- src/test/java/com/endToEnd/Login.java
+++ src/test/java/com/endToEnd/Login.java
@@ -3,6 +3,8 @@ package com.endToEnd;
 import com.framework.BaseFramework;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Login extends BaseFramework {
@@ -11,26 +13,25 @@ public class Login extends BaseFramework {
      * This is a basic login file for creating tests
      */
 
-    static String username = "";
-    static String password = "";
-    String url = "https://www.amazon.com/";
+    String username = " ";
+    String password = " ";
+    String url = "https://www.apple.com/";
 
     @Test
     public void login() throws InterruptedException {
 
         driver.get(url);
 
-        driver.findElement(By.id("nav-link-accountList")).click();
-        driver.findElement(By.className("nav-action-inner")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("ac-gn-bag"))).click();
 
-        driver.findElement(By.id("identifierId")).sendKeys(username);
-        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"ac-gn-bagview-content\"]/nav/ul/li[5]/a"))).click();
 
         Thread.sleep(3000);
-        driver.findElement(By.name("password")).sendKeys(password);
-        driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/span/span")).click();
+        driver.findElement(By.id("recon-0-0")).sendKeys(username);
+        driver.findElement(By.id("recon-0-1")).sendKeys(password);
+        driver.findElement(By.id("signInButtonId")).click();
 
-        assertEquals("https://www.amazon.com/", url);
+        assertEquals("https://www.apple.com/", url);
 
     }
 }
