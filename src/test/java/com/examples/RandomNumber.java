9782d44e06376aec2f81e59456ab1d61b3b7f37c
Kelvin Reid <codesmithk@gmail.com>
2021-05-13 10:31:47 -0400

    Create RandomNumber.java

---
+++ src/test/java/com/examples/RandomNumber.java
@@ -0,0 +1,18 @@
+package com.examples;
+
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main( String args[] ) {
+        Random rand = new Random(); //instance of random class
+        int upperbound = 1000;
+        //generate random values from 0-999
+        int int_random = rand.nextInt(upperbound);
+        double double_random=rand.nextDouble();
+        float float_random=rand.nextFloat();
+
+        System.out.println("Random integer value from 0 to " + (upperbound-1) + " : "+ int_random);
+        System.out.println("Random float value between 0.0 and 1.0 : "+ float_random);
+        System.out.println("Random double value between 0.0 and 1.0 : "+ double_random);
+    }
+}
